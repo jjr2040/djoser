6c0124f6c402ff398902aba161f6603da629c583
Juan Jose Villegas <juan.villegas@dasigno.com>
2017-02-10 11:16:59 -0500

    combined urls

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -12,6 +12,9 @@ base_urlpatterns = (
     url(r'^password/$', views.SetPasswordView.as_view(), name='set_password'),
     url(r'^password/reset/$', views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^password/reset/confirm/$', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    # url(r'^$', views.RootView.as_view(urls_extra_mapping={'login': 'login', 'logout': 'logout'}), name='root'),
 )
 
 urlpatterns = base_urlpatterns + (url(r'^$', views.RootView.as_view(), name='root'),)
